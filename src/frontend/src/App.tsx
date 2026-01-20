5e9342eb43024e3294e64bcb22a7629ed934c63b
WavyWare <wavyware1@proton.me>
2026-01-20 21:32:50 +0100

    Fix code i left unused

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,14 +1,10 @@
 import {MainPage} from "./MainPage.tsx";
-import {FileUpload} from "./FileUpload.tsx";
-import {Done} from "./Done.tsx";
 
 function App() {
 
   return (
       <>
           <MainPage />
-          {(page === "upload" ? <FileUpload/> : null)}
-          {(page === "done" ? <Done filename={filename}/> : null)}
       </>
   );
 }
